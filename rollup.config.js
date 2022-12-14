7bfae304afac9b9298647ad8d072bd54d35e7df4
Citar_ <citarcilofficial@gmail.com>
2022-12-14 05:50:36 +0100

    Init project

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import scss from "rollup-plugin-scss";
+import commonjs from "@rollup/plugin-commonjs";
+import vue from "rollup-plugin-vue";
+
+import pkg from "./package.json" assert { type: "json" };
+
+export default [
+  // ESM build to be used with webpack/rollup.
+  {
+    input: "src/index.js",
+    output: {
+      format: "esm",
+      file: pkg.module,
+    },
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      scss({ fileName: "bundle.css" }),
+      vue(),
+    ],
+  },
+  // SSR build.
+  {
+    input: "src/index.js",
+    output: {
+      format: "cjs",
+      file: pkg.main,
+    },
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      scss({ fileName: "bundle.css" }),
+      vue({ template: { optimizeSSR: true } }),
+    ],
+  },
+  // Browser build.
+  {
+    input: "src/wrapper.js",
+    output: {
+      format: "iife",
+      file: pkg.browser,
+    },
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      scss({ fileName: "bundle.css" }),
+      vue(),
+    ],
+  },
+];
---
+++ src/SampleComponent/SampleComponent.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  setup(props) {
+    const { title, texte, button } = toRefs(props);
+  },
+};
+</script>
+
+<template>
+  <div class="my-component">
+    <h1>{{ title }}</h1>
+    <p>{{ texte }}</p>
+
+    <button>{{ button }}</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.my-component {
+  h1 {
+    color: red;
+  }
+
+  p {
+    color: blue;
+  }
+
+  button {
+    margin-top: 20px;
+    width: 30px;
+    height: 10px;
+  }
+}
+</style>
